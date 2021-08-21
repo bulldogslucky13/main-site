b0f1a2f5ee62699920e7c00cb051ce9dcd4473ea
Cameron Bristol <cameron.bristol@handsdowndigital.com>
2021-08-21 13:55:19 -0500

    Changed default import rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,43 @@
-export const parserOptions = { project: "./tsconfig.eslint.json" };
-export const settings = {
-  "import/resolver": {
-    alias: {
-      map: [["~", "./src"]],
-      extensions: [".js", ".jsx", ".ts", ".tsx", ".mjs"],
+module.exports = {
+  extends: ["plugin:@next/next/recommended"],
+  parserOptions: { project: "./tsconfig.eslint.json" },
+  settings: {
+    "import/resolver": {
+      alias: {
+        map: [["~", "./src"]],
+        extensions: [".js", ".jsx", ".ts", ".tsx", ".mjs"],
+      },
     },
   },
-};
-export const overrides = [
-  {
-    files: [
-      "*.stories.tsx",
-      "src/pages/**/*.ts",
-      "src/pages/**/*.tsx",
-      "src/__mocks__/**/*.ts",
-    ],
-    rules: {
-      "import/no-default-export": "off",
-    },
-  },
-  {
-    files: ["server.js"],
-    rules: {
-      "import/no-extraneous-dependencies": "off",
-      "no-console": "off",
-    },
-  },
-];
-export const rules = {
-  "no-restricted-imports": [
-    "error",
+  overrides: [
     {
-      patterns: ["../*"],
+      files: [
+        "*.stories.tsx",
+        "src/pages/**/*.ts",
+        "src/pages/**/*.tsx",
+        "src/__mocks__/**/*.ts",
+      ],
+      rules: {
+        "import/no-default-export": "off",
+      },
+    },
+    {
+      files: ["server.js"],
+      rules: {
+        "import/no-extraneous-dependencies": "off",
+        "no-console": "off",
+      },
     },
   ],
-  "sonarjs/cognitive-complexity": "off",
-  "sonarjs/no-duplicate-string": "off",
-  complexity: "off",
+  rules: {
+    "no-restricted-imports": [
+      "error",
+      {
+        patterns: ["../*"],
+      },
+    ],
+    "sonarjs/cognitive-complexity": "off",
+    "sonarjs/no-duplicate-string": "off",
+    complexity: "off",
+  },
 };
