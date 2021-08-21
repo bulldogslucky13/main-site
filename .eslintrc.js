e6e1527255785f2f1c08e2e0eb1fb6fc30d61353
Cameron Bristol <cameron.bristol@handsdowndigital.com>
2021-08-21 14:08:30 -0500

    Added prettier rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,43 +1,39 @@
 module.exports = {
-  extends: ["plugin:@next/next/recommended"],
-  parserOptions: { project: "./tsconfig.eslint.json" },
-  settings: {
-    "import/resolver": {
-      alias: {
-        map: [["~", "./src"]],
-        extensions: [".js", ".jsx", ".ts", ".tsx", ".mjs"],
-      },
-    },
-  },
-  overrides: [
-    {
-      files: [
-        "*.stories.tsx",
-        "src/pages/**/*.ts",
-        "src/pages/**/*.tsx",
-        "src/__mocks__/**/*.ts",
-      ],
-      rules: {
-        "import/no-default-export": "off",
-      },
-    },
-    {
-      files: ["server.js"],
-      rules: {
-        "import/no-extraneous-dependencies": "off",
-        "no-console": "off",
-      },
-    },
-  ],
-  rules: {
-    "no-restricted-imports": [
-      "error",
-      {
-        patterns: ["../*"],
-      },
-    ],
-    "sonarjs/cognitive-complexity": "off",
-    "sonarjs/no-duplicate-string": "off",
-    complexity: "off",
-  },
+	extends: ['plugin:@next/next/recommended'],
+	parser: 'babel-eslint',
+	parserOptions: { project: './tsconfig.eslint.json' },
+	settings: {
+		'import/resolver': {
+			alias: {
+				map: [['~', './src']],
+				extensions: ['.js', '.jsx', '.ts', '.tsx', '.mjs'],
+			},
+		},
+	},
+	overrides: [
+		{
+			files: ['*.stories.tsx', 'src/pages/**/*.ts', 'src/pages/**/*.tsx', 'src/__mocks__/**/*.ts'],
+			rules: {
+				'import/no-default-export': 'off',
+			},
+		},
+		{
+			files: ['server.js'],
+			rules: {
+				'import/no-extraneous-dependencies': 'off',
+				'no-console': 'off',
+			},
+		},
+	],
+	rules: {
+		'no-restricted-imports': [
+			'error',
+			{
+				patterns: ['../*'],
+			},
+		],
+		'sonarjs/cognitive-complexity': 'off',
+		'sonarjs/no-duplicate-string': 'off',
+		complexity: 'off',
+	},
 };
