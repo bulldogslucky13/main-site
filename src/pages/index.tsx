e12b7dd179dad719b060253f65c18894a76a65a0
Cameron Bristol <cbristol28@yahoo.com>
2025-01-16 10:48:38 -0600

    Add return to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,14 @@ import Head from 'next/head';
 import { HomePage } from '~/client/domains/portfolio/home-page/home-page';
 
 export default function Home() {
-	<>
-		<Head>
-			<title>Cameron Bristol - Product & Software Engineer</title>
-			<meta name="viewport" content="initial-scale=1.0, width=device-width" />
-			<meta name="p:domain_verify" content="9ceef33f0d4330c15f094a16314f3ba1" />
-		</Head>
-		<HomePage />
-	</>;
+	return (
+		<>
+			<Head>
+				<title>Cameron Bristol - Product & Software Engineer</title>
+				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+				<meta name="p:domain_verify" content="9ceef33f0d4330c15f094a16314f3ba1" />
+			</Head>
+			<HomePage />
+		</>
+	);
 }
