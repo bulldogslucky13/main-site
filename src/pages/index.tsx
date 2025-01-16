184a208e22a6ab61abfff26ac8995c36b8129c8a
Cameron Bristol <cbristol28@yahoo.com>
2025-01-16 10:41:11 -0600

    Update homepage head

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,13 @@
-import type { NextPage } from 'next';
+import Head from 'next/head';
 import { HomePage } from '~/client/domains/portfolio/home-page/home-page';
 
-const Home: NextPage = HomePage;
-export default Home;
+export default function Home() {
+	<>
+		<Head>
+			<title>Cameron Bristol - Product & Software Engineer</title>
+			<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+			<meta name="p:domain_verify" content="9ceef33f0d4330c15f094a16314f3ba1" />
+		</Head>
+		<HomePage />
+	</>;
+}
